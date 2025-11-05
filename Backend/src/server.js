6c48779d63000d5ff84733b8625deb7a99b37fc1
kartik-hub-enjay <kartikpareek6789@gmail.com>
2025-11-05 17:13:23 +0530

    second commit

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -6,13 +6,10 @@ import chatRouter from "./routes/chatRoute.js";
 import {connectDB} from "./lib/db.js"
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
 dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
-
-const __dirname = path.resolve();
 
 app.use(cors({
     origin:  "http://localhost:5173",
@@ -26,12 +23,6 @@ app.use("/api/auth",authRouter);
 app.use("/api/users",userRouter);
 app.use("/api/chat",chatRouter);
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-    })
-}
 
 app.listen(PORT,()=>{
     console.log(`server started on port: ${PORT}`);
